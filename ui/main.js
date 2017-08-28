95ec568013a30ecb069725dbbee8b710ba00b9bd
pjananth22123 <pjananth22123@gmail.com>
2017-08-28 23:14:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,14 +2,15 @@ console.log('Loaded!');
 
 window.onload = clickchecker();
 
+function clickcount(){
+    clickcounter = clickcounter + 1;
+    timespan = document.getElementById('times');
+    timespan.innerHTML = clickcounter.toString();
+}
+
 function clickchecker() {
 button = document.getElementById('click');
-timespan = document.getElementById('times');
 clickcounter = 0;
 button.onclick = clickcount;
 }
 
-function clickcount(){
-    clickcounter = clickcounter + 1;
-    timespan.innerHTML = clickcounter.toString();
-}
