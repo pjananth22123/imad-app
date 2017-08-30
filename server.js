5e1d18b316237e0fc3d7b6e1bde0dede8ec3f2d0
pjananth22123 <pjananth22123@gmail.com>
2017-08-30 08:35:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,6 +147,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/articles/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
