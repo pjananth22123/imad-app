e9de5cba120b8552dae66c931da7d033ea9643c6
pjananth22123 <pjananth22123@gmail.com>
2017-08-30 08:19:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/test-db',function(req,res){
    
-   pool.query('select * from testdata',function(err,results){
+   pool.query('select id,name from testdata',function(err,results){
        
       if(err) {
           res.status(505).send(err.toString());
